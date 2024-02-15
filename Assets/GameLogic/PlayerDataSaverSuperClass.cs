d333717356db3d2cf922ba0f633dbc3f92fd40d3
DELL_LapTop_RenzoSuganuma <discoverygworks2023@gmail.com>
2024-02-15 22:11:44 +0900

    [add / update] add cs files

---
+++ Assets/GameLogic/PlayerDataSaverSuperClass.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TeamC
+{
+    /// <summary> プレイヤーのセーブデータ管理コンポーネントのSuperクラス </summary>
+    public class PlayerDataSaverSuperClass : MonoBehaviour
+    {
+    
+    }
+}
