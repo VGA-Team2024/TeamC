2479e7c972960338c5547e8603294c211f9e006e
DELL_LapTop_RenzoSuganuma <discoverygworks2023@gmail.com>
2024-02-15 23:40:39 +0900

    [add / update] 各コンポーネントの派生クラスを実装中

---
+++ Assets/GameLogic/TesterDerivedClass.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TeamC
+{
+    /// <summary> Superクラスを継承した時の挙動のテスタークラス </summary>
+    public class TesterDerivedClass : MonoBehaviour
+    {
+    }
+}
