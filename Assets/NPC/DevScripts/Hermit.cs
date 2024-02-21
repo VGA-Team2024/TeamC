e21413c9684af307767987cce6b54c34ad47545d
GotouTamaki <134473633+GotouTamaki@users.noreply.github.com>
2024-02-22 00:16:54 +0900

    [add]各種スキルの実装 [update]各種スキルの実装に伴う仙人の効果の調整 [clean]テストデータのスクリプトがNoneになっていたので再生成

--- Assets/NPC/DevScripts/Hermit.cs
+++ Assets/NPC/DevScripts/Hermit.cs
@@ -1,12 +1,33 @@
+using System.Collections.Generic;
+using System.Linq;
+
 namespace TeamC
 {
     /// <summary>仙人の処理</summary>
-    public class Hermit : NPC
+    public class Hermit : NPC, IHermit
     {
         private void FixedUpdate()
         {
             //throw new NotImplementedException();
             base.GetNPCEffects.Invoke();
+        }
+
+        public List<SkillsDataTemplate> GetFirableSkills()
+        {
+            //throw new System.NotImplementedException();
+
+            List<SkillsDataTemplate> skillList = FindObjectsOfType<SkillsDataTemplate>().ToList();
+            List<SkillsDataTemplate> result = new List<SkillsDataTemplate>();
+
+            foreach (var skill in skillList)
+            {
+                if (!skill.IsLocked)
+                {
+                    result.Add(skill);
+                }
+            }
+
+            return result;
         }
     }
 }
---
+++ Assets/Skills/DevScripts/Steal.cs
@@ -0,0 +1,15 @@
+namespace TeamC
+{
+    public class Steal : SkillsSuperClass
+    {
+        /// <summary>スキルロックののフラグを取得します</summary>
+        public bool GetIsLocked() => GetSkillData.IsLocked;
+
+        private void FixedUpdate()
+        {
+            if (GetSkillData.IsLocked) return;
+
+            base.CoolTime();
+        }
+    }
+}
