bfbb49e815f782aa87ac9b25d715d05f0f73f9bc
KakinumaYuzuki <V.proYuzu23@gmail.com>
2024-10-18 07:03:27 +0900

    [fix] スペルミス修正、変数の修正

--- Assets/Scripts/Map/BackgroundMover.cs
+++ Assets/Scripts/Map/BackgroundMover.cs
@@ -1,17 +1,17 @@
 using UnityEngine;
 using UnityEngine.UI;
 
-public class BackgroudMover : MonoBehaviour
+public class BackgroundMover : MonoBehaviour
 {
     private const float MaxLength = 1f;
-    private const string PropName = "_MainTex";
+    private static readonly int MainTex = Shader.PropertyToID("_MainTex");
 
     [SerializeField, Header("スクロール速度")] private Vector3 _offsetSpeed;
     [SerializeField] private GameObject _player;    // 何かしらの方法でプレイヤーを取得する
     
     private Material _material;
     private Vector3 _prevPlayerPos; // 1フレーム前の位置
-    
+
     private void Start()
     {
         // イメージコンポーネントからマテリアルを取得
@@ -35,13 +35,13 @@ public class BackgroudMover : MonoBehaviour
             Vector3 deltaPosition = currentPlayerPos - _prevPlayerPos;
 
             // 差分に基づいてテクスチャのオフセットを計算
-            var x = Mathf.Repeat(_material.GetTextureOffset(PropName).x
+            var x = Mathf.Repeat(_material.GetTextureOffset(MainTex).x
                                  + deltaPosition.x * _offsetSpeed.x, MaxLength);
-            var y = Mathf.Repeat(_material.GetTextureOffset(PropName).y
+            var y = Mathf.Repeat(_material.GetTextureOffset(MainTex).y
                                  + deltaPosition.y * _offsetSpeed.y, MaxLength);
 
             // テクスチャのオフセットを更新
-            _material.SetTextureOffset(PropName, new Vector2(x, y));
+            _material.SetTextureOffset(MainTex, new Vector2(x, y));
 
             // 現在の位置を前回の位置として保存
             _prevPlayerPos = currentPlayerPos;
@@ -54,7 +54,7 @@ public class BackgroudMover : MonoBehaviour
         // ゲームをやめた後にマテリアルのOffsetを戻しておく
         if (_material)
         {
-            _material.SetTextureOffset(PropName, Vector2.zero);
+            _material.SetTextureOffset(MainTex, Vector2.zero);
         }
     }
 }
