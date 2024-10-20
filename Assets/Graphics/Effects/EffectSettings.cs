d64ea7c66b5cf65e80848754ccfd9090a5a01d94
tejimatsuyoshi <tejima.tuyoshi@icloud.com>
2024-10-20 11:22:44 +0900

    斬撃エフェクト(仮)とSetting(仮)を作成。(ignoreは素材を見えないようにしています。)

---
+++ Assets/Graphics/Effects/EffectSettings.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EffectSettings : MonoBehaviour
+{
+    string HitEffectName;
+    // Start is called before the first frame update
+    void Start()
+    {
+        HitEffectName = gameObject.name;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
