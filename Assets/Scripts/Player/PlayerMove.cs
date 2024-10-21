ab87159bcda351ce4a9cc7a70ec87f62bf18e366
kuromaaaaa <kuromaxa@gmail.com>
2024-10-22 00:39:20 +0900

    [Update] レビュー反映

--- Assets/Scripts/Player/PlayerMove.cs
+++ Assets/Scripts/Player/PlayerMove.cs
@@ -4,21 +4,21 @@ using Cysharp.Threading.Tasks;
 
 public class PlayerMove : MonoBehaviour ,IDamageable
 {
-    Rigidbody _rb;
-    PlayerControls _controls;
-    Cinemachine.CinemachineImpulseSource _source;
-    SpriteRenderer _sr;
+    private Rigidbody _rb;
+    private PlayerControls _controls;
+    private Cinemachine.CinemachineImpulseSource _source;
+    private SpriteRenderer _sr;
 
-    Vector2 _dir; //ActionMapのMoveの値を保存するVector2
-    [SerializeField, Header("プレイヤーの移動速度")] float _moveSpeed = 2;
-    [SerializeField, Header("プレイヤーのジャンプ力")] float _jumpPower = 5;
-    [SerializeField, Header("攻撃時に出すゲームオブジェクト")] GameObject _attackCollider;
-    [SerializeField, Header("着地判定用Rayの長さ")] float _rayLength = 0.55f;
-    [SerializeField, Header("ダメージを受けた時に吹き飛ぶ力")] float _knockbackPower = 20;
+    private Vector2 _dir; //ActionMapのMoveの値を保存するVector2
+    [SerializeField, Header("プレイヤーの移動速度")] private float _moveSpeed = 2;
+    [SerializeField, Header("プレイヤーのジャンプ力")] private float _jumpPower = 5;
+    [SerializeField, Header("攻撃時に出すゲームオブジェクト")] private GameObject _attackCollider;
+    [SerializeField, Header("着地判定用Rayの長さ")] private float _rayLength = 0.55f;
+    [SerializeField, Header("ダメージを受けた時に吹き飛ぶ力")] private float _knockbackPower = 20;
 
     //プレイヤーがどちら側を向いているか
-    bool _dirRight = true;
-    bool _playerFlip 
+    private bool _dirRight = true;
+    private bool _playerFlip 
     {
         get => _dirRight;
         set 
@@ -27,11 +27,11 @@ public class PlayerMove : MonoBehaviour ,IDamageable
             _dirRight = value;
         }
     }
-    bool _isMove = true;
+    private bool _isMove = true;
 
 
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
         _rb = GetComponent<Rigidbody>();
         _source = GetComponent<Cinemachine.CinemachineImpulseSource>();
@@ -69,7 +69,7 @@ public class PlayerMove : MonoBehaviour ,IDamageable
         _controls.Disable();
     }
 
-    void OnJump(InputAction.CallbackContext context)
+    private void OnJump(InputAction.CallbackContext context)
     {
         // 地面についているかの判定
         // タグの判定などをしていないので敵の上に居るときでもジャンプが可能になっている
@@ -80,7 +80,7 @@ public class PlayerMove : MonoBehaviour ,IDamageable
     }
 
 
-    void OnAttack(InputAction.CallbackContext context)
+    private void OnAttack(InputAction.CallbackContext context)
     {
         // 攻撃用当たり判定をアクティブにする
         _attackCollider.SetActive(true);
@@ -88,7 +88,7 @@ public class PlayerMove : MonoBehaviour ,IDamageable
     }
 
     // ActionMapのMove
-    void OnMove(InputAction.CallbackContext context)
+    private void OnMove(InputAction.CallbackContext context)
     {
         _dir = context.ReadValue<Vector2>();
         //今向いている方向と逆に向いたときに攻撃判定用ゲームオブジェクトの位置を変える
