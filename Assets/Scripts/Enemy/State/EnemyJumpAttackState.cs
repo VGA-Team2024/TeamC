f0a3d00a38a69b0395249cef3fcbc557b83daf63
YamamotoU518 <hw37hnmr@gmail.com>
2024-11-15 15:37:54 +0900

    [update] 敵のコライダーの調整 [Add] ボス熊の動きの追加, 追跡をやめる距離の調整機能, ジャンプ攻撃時のスピードの調整機能

--- Assets/Scripts/Enemy/State/EnemyJumpAttackState.cs
+++ Assets/Scripts/Enemy/State/EnemyJumpAttackState.cs
@@ -10,17 +10,18 @@ public class EnemyJumpAttackState : IEnemyState
     private readonly Transform _transform;
     private readonly AnimationCurve _animationCurve;
     private float _curveRate;
-    private float _speed;
+    private readonly float _speed;
     private Vector2 _startPos;
     private float _x;
     private readonly float _duration;
 
-    public EnemyJumpAttackState(EnemyBase enemyBase, EnemyFreezeState freezeState, Animator animator, Transform transform, AnimationCurve animationCurve)
+    public EnemyJumpAttackState(EnemyBase enemyBase, EnemyFreezeState freezeState, Animator animator, Transform transform, float speed, AnimationCurve animationCurve)
     {
         _enemyBase = enemyBase;
         _freezeState = freezeState;
         _animator = animator;
         _transform = transform;
+        _speed = speed;
         _animationCurve = animationCurve;
         _duration = _animationCurve.keys[_animationCurve.length - 1].time;
     }
@@ -30,7 +31,6 @@ public class EnemyJumpAttackState : IEnemyState
         if (_animator) _animator.SetTrigger(_jump);
         _startPos = _transform.position;
         _x = 0f;
-        _speed = 1f;
     }
 
     public void Execute()
@@ -50,13 +50,8 @@ public class EnemyJumpAttackState : IEnemyState
     
     private void Attack()
     {
-        _curveRate = Mathf.Clamp(_curveRate + _speed * Time.deltaTime, 0f, 1f);
-        _x += Time.deltaTime;
-        var y = _animationCurve.Evaluate(_curveRate);
+        _x += Time.deltaTime * Mathf.Abs(_speed);
+        var y = _animationCurve.Evaluate(_x);
         _transform.position = (Vector3)_startPos + _transform.rotation * new Vector2(_x, y);
-        
-        if(Mathf.Approximately(_curveRate, 1f)){
-            _speed = -1;
-        }
     }
 }
