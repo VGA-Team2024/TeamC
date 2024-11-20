c753f404264736e2a5020e58ae9b9e3e30a91323
YamamotoU518 <hw37hnmr@gmail.com>
2024-11-20 14:37:17 +0900

    [update] 歩行ステートのとき、前が壁か、地面がない場合引き返すようにしました

--- Assets/Scripts/Enemy/State/EnemyWalkState.cs
+++ Assets/Scripts/Enemy/State/EnemyWalkState.cs
@@ -10,9 +10,12 @@ public class EnemyWalkState : IEnemyState
     private readonly float _speed;
     private readonly float _patrolArea;
     private readonly Vector2 _startPos;
-    private float _patrolTimer;
-    private bool _onLastPos;
-
+    
+    private readonly float _rayLength = 2.5f;
+    private readonly Vector3 _rayOffset = new Vector3(0, -0.5f, 0);
+    private readonly Vector2 _rightDir = new Vector2(1, -1).normalized;
+    private readonly Vector2 _leftDir = new Vector2(-1, -1).normalized;
+    
     public EnemyWalkState(EnemyBase enemyBase,Animator animator, Transform transform, float speed, float area)
     {
         _enemyBase = enemyBase;
@@ -22,6 +25,7 @@ public class EnemyWalkState : IEnemyState
         _speed = speed;
         _patrolArea = area;
     }
+    
     
     public void Enter()
     {
@@ -46,6 +50,18 @@ public class EnemyWalkState : IEnemyState
 
     private void Direction()
     {
+        // 前に床がなければ引き返す
+        Vector3 rayOrigin = _transform.position + _rayOffset;
+        bool hit = Physics.Raycast(rayOrigin, _transform.rotation.y > 0 ? _rightDir : _leftDir, out RaycastHit hitInfo, _rayLength);
+
+        // 前が壁なら引き返す
+        bool wallHit = Physics.Raycast(rayOrigin, -_transform.right, out RaycastHit wallHitInfo, _rayLength);
+        
+        if (!hit || LayerMask.LayerToName(hitInfo.transform.gameObject.layer) != "Ground" || wallHit && LayerMask.LayerToName(wallHitInfo.transform.gameObject.layer) == "Ground")
+        {
+            _transform.eulerAngles = new Vector2(0, _transform.eulerAngles.y == 0 ? 180 : 0);
+        }
+        
         if (_transform.position.x <= _startPos.x + 0.01f)
         {
             _transform.eulerAngles = new Vector2(0, 180);
