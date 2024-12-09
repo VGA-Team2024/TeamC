1461e66eeb33478580436c31da748d07cb1d3fa1
KakinumaYuzuki <V.proYuzu23@gmail.com>
2024-12-09 10:27:22 +0900

    [update] テキスト表示用スクリプトをInputSystemに対応

--- Assets/Scripts/Map/StageEvent/InteractUIChanger.cs
+++ Assets/Scripts/Map/StageEvent/InteractUIChanger.cs
@@ -1,28 +1,47 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 /// <summary> 会話開始時のUI切り替え </summary>
 public class InteractUIChanger : MonoBehaviour
 {
-    [SerializeField, Header("インタラクトキー")] private KeyCode _key;
     [SerializeField, Header("会話時の背景パネル")] private GameObject _panel; // 動かないのでUIでもOK
     
-    private void Update()
+    private PlayerControls _controls;
+    private Vector2 _dir; //ActionMapのMoveの値を保存するVector2
+    private const float Threshold = 0.1f;
+    
+    private void Awake()
     {
-        // インタラクトキーが押されたとき
-        if (Input.GetKeyDown(_key))
-        {
-            // 背景パネルをアクティブにする
-            ShowBackgroundPanel();
-        }
-        
-        // ToDo:会話の詳細がきたらシステムを作る(パネル側に別のクラスを作る)
-
+        _controls = new PlayerControls();
+        _controls.InGame.Move.started += ShowBackgroundPanel;
     }
 
-    private void ShowBackgroundPanel()
+    private void OnDestroy()
     {
-        _panel.SetActive(true);
-        // 自身(テキスト)を非表示にする
-        gameObject.SetActive(false);
+        _controls.Dispose();
+        _controls.InGame.Move.started -= ShowBackgroundPanel;
+    }
+
+    private void OnEnable()
+    {
+        _controls.Enable();
+    }
+
+    private void OnDisable()
+    {
+        _controls.Disable();
+    }
+
+    private void ShowBackgroundPanel(InputAction.CallbackContext callbackContext)
+    {
+        _dir = callbackContext.ReadValue<Vector2>();
+        
+        if (_dir.x is < Threshold and > -Threshold && _dir.y > Threshold)
+        { 
+             _panel.SetActive(true);
+             
+             // 自身(テキスト)を非表示にする
+             gameObject.SetActive(false);         
+        }
     }
 }
