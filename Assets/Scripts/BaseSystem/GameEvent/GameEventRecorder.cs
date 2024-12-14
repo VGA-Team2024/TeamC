ad064691a2081bde71ba09b47a9f7f5f87794ad0
k.mitarai <kazmit299@gmail.com>
2024-12-15 00:23:40 +0900

    いったん通信つないだところまで

--- Assets/Scripts/BaseSystem/GameEvent/GameEventRecorder.cs
+++ Assets/Scripts/BaseSystem/GameEvent/GameEventRecorder.cs
@@ -1,7 +1,7 @@
 using System;
-using UnityEditor;
 using UnityEngine;
 using GameEvent;
+using Cysharp.Threading.Tasks;
 
 /// <summary>
 /// ゲームのイベントを記録する
@@ -16,8 +16,36 @@ public class GameEventRecorder
         public string GameHash;
         public DateTime GameStartTime;
         public bool IsReview;
+        public bool IsGameEnd;
     };
 
+    [Serializable]
+    public class CommonEventData
+    {
+        [SerializeField] string EventName;
+        [SerializeField] string TeamID;
+        [SerializeField] string BuildHash;
+        [SerializeField] string GameHash;
+        [SerializeField] int GamePlayTime;
+        [SerializeField] GameEventData Payload;
+
+        private CommonEventData() { }
+        public CommonEventData(string evtName, GameEventData payload)
+        {
+            EventName = evtName;
+            TeamID = BuildState.TeamID;
+            BuildHash = BuildState.BuildHash;
+            if (_instance._currentGame != null)
+            {
+                GameHash = _instance._currentGame.GameHash;
+                GamePlayTime = DateTime.Compare(_instance._currentGame.GameStartTime, DateTime.Now);
+            }
+            Payload = payload;
+        }
+    };
+
+
+    const string baseuri = "https://jyl5w9zfz3.execute-api.ap-northeast-1.amazonaws.com/release/";
     EventRecord _currentGame = null;
 
     /// <summary>
@@ -34,6 +62,8 @@ public class GameEventRecorder
         _instance._currentGame = new EventRecord();
         _instance._currentGame.GameHash = Guid.NewGuid().ToString();
         _instance._currentGame.GameStartTime = DateTime.Now;
+
+        _instance.SendStart();
     }
 
     static public void GameReview(Action reviewEndCallback)
@@ -46,7 +76,7 @@ public class GameEventRecorder
         }
 
         _instance._currentGame.IsReview = true;
-        ReviewWindow.Build(reviewEndCallback);
+        ReviewWindow.Build(_instance.SendReview, reviewEndCallback);
     }
 
     static public void GameEnd(Action reviewEndCallback)
@@ -57,16 +87,48 @@ public class GameEventRecorder
 
             GameStart();
         }
+        
+        _instance._currentGame.IsGameEnd = true;
 
         if (_instance._currentGame.IsReview == false)
         {
             GameReview(reviewEndCallback);
         }
-        _instance._currentGame = null;
+        else
+        {
+            _instance._currentGame = null;
+        }
     }
 
-    static public void SendEventData(GameEventData data)
+    async void SendStart()
     {
+        if (BuildState.BuildHash == "UNITY_EDITOR") return;
 
+        var data = new GameEventData();
+        data.DataPack("Time", _instance._currentGame.GameStartTime);
+        var packet = new CommonEventData("GameStart", data);
+        string json = JsonUtility.ToJson(packet);
+        var res = await Network.WebRequest.PostRequest(baseuri + "Event", packet);
+        Debug.Log(res);
+    }
+
+    async void SendReview(int star, string comment)
+    {
+        var data = new GameEventData();
+        data.DataPack("StarNum", star);
+        data.DataPack("Comment", comment);
+        var packet = new CommonEventData("Review", data);
+        await Network.WebRequest.PostRequest(baseuri + "GameReview", packet);
+
+        if (_instance._currentGame != null && _instance._currentGame.IsGameEnd)
+        {
+            _instance._currentGame = null;
+        }
+    }
+
+    static public async void SendEventData(string situation, GameEventData data)
+    {
+        var packet = new CommonEventData(situation, data);
+        await Network.WebRequest.PostRequest(baseuri + "Event", packet);
     }
 }
--- Assets/Scripts/BaseSystem/GameEvent/ReviewWindow.cs
+++ Assets/Scripts/BaseSystem/GameEvent/ReviewWindow.cs
@@ -1,25 +1,89 @@
 using System;
-using UnityEditor;
 using UnityEngine;
 using UnityEngine.UI;
-using GameEvent;
 using UnityEngine.AddressableAssets;
+using UnityEngine.EventSystems;
+using Cysharp.Threading.Tasks;
 
 /// <summary>
-/// ゲームのイベントを記録する
+/// レビューダイアログ
 /// </summary>
 public class ReviewWindow : MonoBehaviour
 {
+    [SerializeField] InputField _textField;
     [SerializeField] Button _sendButton;
-    Action _sendCallback;
 
-    static public ReviewWindow Build(Action callback)
+    [SerializeField] ReviewStar[] _starts;
+    Action<int, string> _sendFunction;
+    Action _sendCallback;
+    int _currentStar = 0;
+
+
+    void Setup()
     {
-        var obj = Addressables.LoadAssetAsync<GameObject>("Assets/Prefabs/Review.prefab").WaitForCompletion();
-        Utility.Instantiate(obj);
+        //_starts = GetComponentsInChildren<ReviewStar>();
+        _sendButton.enabled = false;
+        foreach (var s in _starts)
+        {
+            int index = s.Index;
+            s.Setup();
+            var evt = s.gameObject.AddComponent<EventTrigger>();
+
+            /*
+            var hover = new EventTrigger.Entry();
+            hover.eventID = EventTriggerType.PointerEnter;
+            hover.callback.AddListener((d) => SetStar(index));
+            evt.triggers.Add(hover);
+            */
+
+            Button btn = s.GetComponent<Button>();
+            btn.onClick.AddListener(() =>
+            {
+                SetStar(index);
+            });
+        }
+    }
+
+    void SetStar(int index)
+    {
+        foreach (var s in _starts)
+        {
+            s.SetStar(s.Index <= index);
+        }
+        _currentStar = index;
+
+        UpdateSendButton();
+    }
+
+    void UpdateSendButton()
+    {
+        if (_currentStar > 0 && !_sendButton.enabled)
+        {
+            _sendButton.enabled = true;
+            _sendButton.onClick.AddListener(SendReview);
+        }
+    }
+
+    void SendReview()
+    {
+        int starNum = _currentStar;
+        string reviewtext = _textField.text;
+        _sendFunction?.Invoke(starNum, reviewtext);
+        _sendCallback?.Invoke();
+    }
+
+
+    //static
+
+    static public ReviewWindow Build(Action<int, string> send, Action callback)
+    {
+        var obj = Addressables.LoadAssetAsync<GameObject>("Assets/Prefabs/Review/Review.prefab").WaitForCompletion();
+        var newObj = Utility.Instantiate(obj);
         Addressables.Release(obj);
-        ReviewWindow review = obj.GetComponent<ReviewWindow>();
+        ReviewWindow review = newObj.GetComponent<ReviewWindow>();
+        review._sendFunction = send;
         review._sendCallback = callback;
+        review.Setup();
 
         return review;
     }
