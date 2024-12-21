5352f6f4fbe367da841fb484e635e70665807be5
ntubasa3582 <134475208+ntubasa3582@users.noreply.github.com>
2024-12-21 13:38:32 +0900

    [add,update]DoorSwitchのスプライト差し替え、DoorSwitch起動時スプライトの変更

--- Assets/Scripts/Map/StageGimmick/DoorSwitch.cs
+++ Assets/Scripts/Map/StageGimmick/DoorSwitch.cs
@@ -13,28 +13,37 @@ public class DoorSwitch : MonoBehaviour ,IDamageable
 {
     [SerializeField,InspectorVariantName("Switch、プレイヤーの攻撃で開く、MusicBox、オルゴールが呼び出されたら開く")] private KeyConditions _keyConditions;
     [SerializeField] GameObject _doorPrefab;
+    [SerializeField] Sprite _switchSprite;
+
+    bool _isSwitch = false;
     /// <summary>ドアを開くメソッド</summary>
     void DoorOpen()
     {
         // TODO ドアが開く処理を書く
         _doorPrefab.SetActive(false);
         CRIAudioManager.BGM.Play("SE_Gimmick", "SE_Gimmick_Door01");
+        _isSwitch = true;
+        gameObject.GetComponent<SpriteRenderer>().sprite = _switchSprite;
     }
     /// <summary>このメソッドが呼ばれたらオブジェクトのアクティブ状態をオンにする</summary>
     void DoorClose()
     {
         _doorPrefab.SetActive(true);
+        _isSwitch = true;
     }
 
     public void TakeDamage(int damage)
     {
-        if (_keyConditions == KeyConditions.DoorOpen)
+        if (!_isSwitch)
         {
-            DoorOpen();
-        }
-        else if (_keyConditions == KeyConditions.MusicBox)
-        {
-            // TODO オルゴールの？処理を書く
+            if (_keyConditions == KeyConditions.DoorOpen)
+            {
+                DoorOpen();
+            }
+            else if (_keyConditions == KeyConditions.MusicBox)
+            {
+                // TODO オルゴールの？処理を書く
+            }
         }
     }
 }
