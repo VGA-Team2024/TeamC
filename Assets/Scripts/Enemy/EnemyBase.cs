168d6956ecd97867af60bf9774edd18253af7720
ntubasa3582 <134475208+ntubasa3582@users.noreply.github.com>
2024-12-23 15:13:32 +0900

    [update]死亡時エフェクトを再生できるようにした

--- Assets/Scripts/Enemy/EnemyBase.cs
+++ Assets/Scripts/Enemy/EnemyBase.cs
@@ -1,7 +1,12 @@
+using System;
+using Unity.Mathematics;
 using UnityEngine;
+using Cysharp.Threading.Tasks;
 
 public class EnemyBase : MonoBehaviour
 {
+    [SerializeField, Header("死亡時に出現するエフェクト")] private GameObject _hoverEffectPrefab;
+    [SerializeField, Header("死亡エフェクトが出るまでの時間")] private float _effectInstanceTime = 0.5f;
     [SerializeField] protected float _speed;
     protected IEnemyState _currentState;
     protected EnemyIdleState _idleState;
@@ -27,11 +32,19 @@ public class EnemyBase : MonoBehaviour
     
     protected virtual void OnUpdate(){}
 
-    public void ChangeState(IEnemyState nextState)
+    public async void ChangeState(IEnemyState nextState)
     {
         if (_currentState != null) _currentState.Exit();
-
+        
         _currentState = nextState;
         _currentState.Enter();
+        if (_isDeath)
+        {
+            await UniTask.Delay(TimeSpan.FromSeconds(_effectInstanceTime));
+            if (_hoverEffectPrefab)
+            {
+                Instantiate(_hoverEffectPrefab, transform.position, quaternion.identity);   
+            }
+        }
     }
 }
