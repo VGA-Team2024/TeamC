76d2e34a95d70ff58f2e9190ede9b17dda26c6bb
k.mitarai <kazmit299@gmail.com>
2024-12-27 15:56:40 +0900

    SceneLauncher改善
    (cherry picked from commit d8cdd5465cb40328bebad33605f45e690e094f1e)

--- Assets/Scripts/BaseSystem/GameSystemTemplate/SceneLauncher.cs
+++ Assets/Scripts/BaseSystem/GameSystemTemplate/SceneLauncher.cs
@@ -13,11 +13,15 @@ public class SceneLauncher : MonoBehaviour
     [SerializeField] bool _isDebug;
     [SerializeField] string _sceneName;
     [SerializeField] GameObject _base;
+    [SerializeField] GameObject _debugRoot;
 
     private void Start()
     {
+#if UNITY_EDITOR
         if (_isDebug)
         {
+            _debugRoot.SetActive(true);
+
             var sceneDB = Addressables.LoadAssetAsync<SceneDependencies>(SceneDependencies.AssetPath).WaitForCompletion();
             var scenes = sceneDB.GetAll().Where(s => s.CanIDebugSelect).ToList();
 
@@ -40,5 +44,8 @@ public class SceneLauncher : MonoBehaviour
             //
             SceneLoader.LoadScene(_sceneName);
         }
+#else
+        SceneLoader.LoadScene(_sceneName);
+#endif
     }
 }
