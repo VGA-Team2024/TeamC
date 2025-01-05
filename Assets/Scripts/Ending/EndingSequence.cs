9cf15960c2259474577bd6bb6ce1ff430b13950b
okabe783 <134473737+okabe783@users.noreply.github.com>
2025-01-05 11:46:03 +0900

    [update] 音がならないバグを修正　Durationを細かく設定できるようにした

--- Assets/Scripts/Ending/EndingSequence.cs
+++ Assets/Scripts/Ending/EndingSequence.cs
@@ -1,6 +1,7 @@
 using System;
 using Cysharp.Threading.Tasks;
 using UnityEngine;
+using UnityEngine.Serialization;
 using UnityEngine.UI;
 
 namespace Ending
@@ -14,29 +15,35 @@ namespace Ending
         
         [SerializeField, InspectorVariantName("エンディングを表示する間隔")] private float[] _endingFadeDuration;
         [SerializeField,InspectorVariantName("クレジットを表示する間隔")] private float[] _fadeCreditDuration;
-        [SerializeField, InspectorVariantName("流したいBGM")] private string _bgmName;
+        [SerializeField, InspectorVariantName("Ending再生時に流したいBGM")] private string _endingBgmName;
         [SerializeField, InspectorVariantName("Credit再生時に流したいBGM")] private string _creditBgmName;
         [SerializeField, InspectorVariantName("表示させたい一枚絵のリスト")] private Sprite[] _endingSpriteLists;
         [SerializeField, InspectorVariantName("表示させたいクレジットイラストのリスト")] private Sprite[] _creditSpriteLists;
 
         [SerializeField, InspectorVariantName("エンディングとクレジットの間")] private float _delay;
         
-        [SerializeField] private float _fadeDuration;
+        [SerializeField,InspectorVariantName("EndingのFadeoutの時間")] private float _fadeoutDuration;
+        [SerializeField,InspectorVariantName("EndingのFadeinの時間")] private float _fadeinDuration;
+        [SerializeField,InspectorVariantName("クレジット画面のFadeoutの時間")] private float _fadeOutCreditDuration;
+        [SerializeField,InspectorVariantName("クレジット画面のFadeinの時間")] private float _fadeInCreditDuration;
+
+        [SerializeField, InspectorVariantName("タイトル遷移前のFade時間")] private float _finalDuration;
         #endregion
 
         private const int _musicIndex = 3;
         private CRIAudioManager.SoundPlayer _musicBoxPlayer;
         private Credit _credit;
 
-        private void Awake()
+        private void Start()
         {
             Initialize();
         }
 
         private void Initialize()
         {
+            CRIAudioManager.Initialize();
             _credit = new Credit();
-            _credit.Initialize(_fadeController, _backGround, _creditSpriteLists);
+            _credit.Initialize(_fadeController,_fadeOutCreditDuration,_fadeInCreditDuration, _backGround, _creditSpriteLists);
             _musicBoxPlayer = new CRIAudioManager.SoundPlayer(SoundType.BGM);
             _musicBoxPlayer.Setup();
             _musicBoxPlayer.SetVolume(1.0f);
@@ -45,13 +52,14 @@ namespace Ending
 
         public async UniTask PlayEnding()
         {
-            _musicBoxPlayer.Play("MusicBox", _creditBgmName);
+            _musicBoxPlayer.Play("BGM", _endingBgmName);
             await ShowEndingMovie();
+            await UniTask.Delay(TimeSpan.FromSeconds(_delay));
             _musicBoxPlayer.Stop();
-            _musicBoxPlayer.Play("BGM", _bgmName);
-            await _credit.ShowCredit(_delay,_fadeCreditDuration);
+            _musicBoxPlayer.Play("MusicBox", _creditBgmName);
+            await _credit.ShowCredit(_fadeCreditDuration);
             _musicBoxPlayer.Stop();
-            await StartFadeOut();
+            await StartFadeOut(_finalDuration);
             SceneLoader.LoadScene("01_Title");
         }
 
@@ -59,7 +67,8 @@ namespace Ending
         // 一定間隔でイラストがながれていく(4枚)　切り換えるときもFadeで切り換え
         private　async UniTask ShowEndingMovie()
         {
-            await StartFadeOut();
+            // エンディングの始まりにFadeする
+            await StartFadeOut(_fadeoutDuration);
             _backGround.gameObject.SetActive(true);
             // 一枚絵を切り替える
             for (int i = 0; i < _endingSpriteLists.Length; i++)
@@ -67,29 +76,28 @@ namespace Ending
                 // Fadeの処理
                 if (i != 0)
                 {
-                    await StartFadeOut();
+                    // 始まりでFadeをしているので2回目から
+                    await StartFadeOut(_fadeoutDuration);
                 }
                 
                 _backGround.sprite = _endingSpriteLists[i];
                 // 表示間隔分待つ
                 await UniTask.Delay(TimeSpan.FromSeconds(_endingFadeDuration[i]));
                 // Spriteの切り替え
-                await StartFadeIn();
+                await StartFadeIn(_fadeinDuration);
             }
 
-            await StartFadeOut();
+            await StartFadeOut(_fadeoutDuration);
         }
         
-        // 最後に黒い画面へフェードアウト
-        private async UniTask StartFadeIn()
+        private async UniTask StartFadeIn(float duration)
         {
-            await _fadeController.FadeInAsync(_fadeDuration);
+            await _fadeController.FadeInAsync(duration);
         }
-
-        // 最後に黒い画面へフェードアウト
-        private async UniTask StartFadeOut()
+        
+        private async UniTask StartFadeOut(float duration)
         {
-            await _fadeController.FadeOutAsync(_fadeDuration);
+            await _fadeController.FadeOutAsync(duration);
         }
     }
 }
