f86c9ad2c719de4edc67e6d33c189b2120190438
okabe783 <134473737+okabe783@users.noreply.github.com>
2025-02-10 09:30:26 +0900

    [update] 動画の編集

--- Assets/Scripts/Title/OpeningTitleText.cs
+++ Assets/Scripts/Title/OpeningTitleText.cs
@@ -6,31 +6,31 @@ using UnityEngine;
 namespace Title
 {
     public class OpeningTitleText : MonoBehaviour
-    {
-        [SerializeField, InspectorVariantName("表示したいテキスト昇順")] private string[] _subTitle;
-        [SerializeField,InspectorVariantName("テキストを表示する間隔")] private float[] _textDuration;
-        [SerializeField,InspectorVariantName("テキストコンポーネント")] private TextMeshProUGUI _text;
-        
-        public async UniTask ShowTitle()
-        {
-            if (_subTitle.Length != _textDuration.Length)
-            {
-                Debug.LogError("テキストの数と間隔の数が一致していません");
-                return;
-            }
-
-            _text.gameObject.SetActive(true);
-            
-            // テキストを順番に表示する
-            for (int i = 0; i < _subTitle.Length; i++)
-            {
-                // テキストの間隔分待つ
-                await UniTask.Delay(TimeSpan.FromSeconds(_textDuration[i]));
-                _text.text = _subTitle[i]; 
-            }
-
-            await UniTask.Delay(1000);
-            _text.gameObject.SetActive(false);
-        }
-    }
-}
+     {
+         [SerializeField, InspectorVariantName("表示したいテキスト昇順")] private string[] _subTitle;
+         [SerializeField,InspectorVariantName("テキストを表示する間隔")] private float[] _textDuration;
+         [SerializeField,InspectorVariantName("テキストコンポーネント")] private TextMeshProUGUI _text;
+         
+         public async UniTask ShowTitle()
+         {
+             if (_subTitle.Length != _textDuration.Length)
+             {
+                 Debug.LogError("テキストの数と間隔の数が一致していません");
+                 return;
+             }
+ 
+             _text.gameObject.SetActive(true);
+             
+             // テキストを順番に表示する
+             for (int i = 0; i < _subTitle.Length; i++)
+             {
+                 // テキストの間隔分待つ
+                 await UniTask.Delay(TimeSpan.FromSeconds(_textDuration[i]));
+                 _text.text = _subTitle[i]; 
+             }
+ 
+             await UniTask.Delay(1000);
+             _text.gameObject.SetActive(false);
+         }
+     }
+ }
